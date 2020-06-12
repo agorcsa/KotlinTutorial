ff76143d0169b1a946d732f33df82e77140ecabc
andreeagorcsa <andreea.gorcsa@t-systems.com>
2020-06-12 22:01:27 +0200

    Solved warnings: 1. refactored some "var"s into "val"s 2. merged variable declaration with variable value assignment

--- app/src/main/java/Main.kt
+++ app/src/main/java/Main.kt
@@ -1,28 +1,32 @@
-fun main(args: Array<String>) {
+fun main() {
     println("Hello World")
     println("My first Kotlin program")
 
-    var name: String
-    name = "Andreea"
+    val name = "Andreea"
     println("My name is $name")
 
-    var weeklySalary: Int = 1000
-    var monthlySalary: Int = weeklySalary * 4;
+    val weeklySalary = 1000
+    val monthlySalary: Int = weeklySalary * 4
 
     println("The weekly salary is $weeklySalary")
     println("The monthly salary is ${weeklySalary * 4}, which is $weeklySalary * 4")
 
-    val oranges: Int = 5
-    val apples: Int = 6
-    var fruit: Int = oranges + apples
+    val oranges = 5
+    val apples = 6
+    val fruit: Int = oranges + apples
 
     println("$oranges oranges + $apples apples = $fruit pieces of fruit")
     println("A qaurter of all the fruits is equal to ${fruit/4}")
     println("A quarter of the apples is ${apples / 4}")
 
-    val weeks: Int = 234
+    val weeks = 234
     val years: Double = weeks / 52.0
     println("$weeks weeks in years is $years")
 
+    val jim = "James T.Kirk"
+
     println("My name is $name")
+    println("The captain of the enterprise is $jim")
+    println("I can print \$name")
+    println("My monthly salary is $monthlySalary$")
 }
