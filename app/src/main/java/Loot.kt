a0cc9972ca712fe996f14463c63388e4d3bc3136
andreeagorcsa <andreea.gorcsa@t-systems.com>
2020-06-14 19:11:12 +0200

    New object class created "Loot"

---
+++ app/src/main/java/Loot.kt
@@ -0,0 +1,14 @@
+
+enum class LootType {
+    POTION, RING, ARMOR
+}
+class Loot(val name: String, val type: LootType, val value: Double) {
+
+    override fun toString(): String {
+        return """
+            loot name: $name
+            loot type: $type
+            loot value: $value
+        """
+    }
+}
