805a1ee52445e9933cf24912aa06b6e78011862a
andreeagorcsa <andreea.gorcsa@t-systems.com>
2020-06-14 19:12:42 +0200

    toSiring() used to display the "Player" attributes

--- app/src/main/java/Player.kt
+++ app/src/main/java/Player.kt
@@ -2,14 +2,32 @@ class Player(val name: String, var level: Int = 1, var lives: Int = 3, var score
 
     var weapon: Weapon = Weapon("Fist", 1)
 
+    // ArrayList
+    val inventory = ArrayList<Loot>()
+
+    fun showInventory() {
+        println("$name's inventory")
+        println(inventory.get(0))
+        println("______________")
+    }
+
     fun show() {
-        println("""
+       if (lives > 0) {
+           println("$name is alive")
+       } else {
+           println("$name is dead")
+       }
+    }
+
+    // returns a String
+    // Kotlin uses the overwritten toString() fun to display the players
+    override fun toString(): String {
+        return """
         name: $name
         lives: $lives
         level: $level
         score: $score
-        weapon name: ${weapon.name}
-        weapon damageInflicted: ${weapon.damageInflicted}
-        """)
+        weapon: $weapon
+        """
     }
 }
